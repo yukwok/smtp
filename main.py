603e715eea2c1c1e8bf83030d898c157c4b8d2de
Yukwok <billynyk@gmail.com>
2021-06-08 22:39:41 +0800

    smtp test complete

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import smtplib
 
 my_email = "yukwokng@yahoo.com"
-password="abcde12345()"
+password="vmpfjhflkzrtspzp"
 
-connection = smtplib.SMTP("smtp.mail.yahoo.com")
-print(f'connection: {connection}')
-print( connection.starttls())
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="billynyk@gmail.com", msg="testing")
-connection.close()
+with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs="billynyk@gmail.com",
+                        msg="Subject:testing\n\nThis is the content body.")
 
 
