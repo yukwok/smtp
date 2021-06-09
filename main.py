bebbf7a7029adfe3bc6474367360008270a11e3c
Yukwok <billynyk@gmail.com>
2021-06-09 08:58:08 +0800

    Read the text gile of famous quotes, and send through smtp

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+
 import smtplib
+import datetime as dt
+import random
+
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+with open("quotes.txt") as quote_file:
+    all_quotes = quote_file.readlines()
+    quote = random.choice(all_quotes)
+
+# print(quote)
 
 my_email = "yukwokng@yahoo.com"
 password="vmpfjhflkzrtspzp"
@@ -8,6 +21,35 @@ with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
     connection.login(user=my_email, password=password)
     connection.sendmail(from_addr=my_email,
                         to_addrs="billynyk@gmail.com",
-                        msg="Subject:testing\n\nThis is the content body.")
+                        msg=f"Subject:famous string\n\n{quote}")
+
+
+
+
+
+
+
+
+
+
+# import smtplib
+#
+# my_email = "yukwokng@yahoo.com"
+# password="vmpfjhflkzrtspzp"
+#
+# with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs="billynyk@gmail.com",
+#                         msg="Subject:testing\n\nThis is the second try"
+#                             "content body.")
+#
+#
+# # import datetime as dt
+# #
+# now = dt.datetime.now()
+# print(now)
+
 
 
