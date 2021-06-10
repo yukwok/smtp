8589ae802baf89e40efa0cc6662732960d59b7fc
Yukwok <billynyk@gmail.com>
2021-06-10 12:40:36 +0800

    birthday greeting update

--- main.py
+++ main.py
@@ -3,37 +3,50 @@ import smtplib
 import datetime as dt
 import random
 
+import pandas
 
-now = dt.datetime.now()
-weekday = now.weekday()
+my_email = "billy_smtp@yahoo.com"
+password="teuaoltgmzaykgnx"
 
-with open("quotes.txt") as quote_file:
-    all_quotes = quote_file.readlines()
-    quote = random.choice(all_quotes)
+# Today -----
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
+
+# name,email,year,month,day
+# Test,test@email.com,1961,12,21
+# [Fill this in!]
+
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+
+
+if  today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    # print(birthday_person)
+    # print(birthday_person["name"])
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    # print(file_path)
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="billynyk@qq.com",
+                            msg=f"Subject:Happy birthday !"
+                            f"\n\n"
+                            f"{contents}")
+
+        #
+
+#     all_quotes = quote_file.readlines()
+#     quote = random.choice(all_quotes)
 
 # print(quote)
 
-my_email = "yukwokng@yahoo.com"
-password="vmpfjhflkzrtspzp"
-
-with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="billynyk@gmail.com",
-                        msg=f"Subject:famous string\n\n{quote}")
-
-
-
-
-
-
-
-
-
-
-# import smtplib
-#
 # my_email = "yukwokng@yahoo.com"
 # password="vmpfjhflkzrtspzp"
 #
@@ -42,14 +55,15 @@ with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
 #     connection.login(user=my_email, password=password)
 #     connection.sendmail(from_addr=my_email,
 #                         to_addrs="billynyk@gmail.com",
-#                         msg="Subject:testing\n\nThis is the second try"
-#                             "content body.")
+#                         msg=f"Subject:famous string\n\n{quote}")
 #
-#
-# # import datetime as dt
-# #
-# now = dt.datetime.now()
-# print(now)
+
+
+
+
+
+
+
 
 
 
